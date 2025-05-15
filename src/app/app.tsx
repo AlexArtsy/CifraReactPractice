2288f966acd95375b39fa9c9ce09fc05a0b19085
Арцыбашев Александр (Artsybashev_AY) <Artsybashev_AY@directum.ru>
2025-05-15 18:58:54 +0400

    Заготовка для Vite

---
+++ src/app/app.tsx
@@ -0,0 +1,10 @@
+// import { Button } from '@/components/ui/button/button';
+
+export const App = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Hello Vite + React + TS + Tailwind + Emotion!</h1>
+      {/* <Button onClick={() => alert('Clicked!')}>Click me</Button> */}
+    </div>
+  );
+};
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  server: {
+    port: 3003,
+    open: true,
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
+});
